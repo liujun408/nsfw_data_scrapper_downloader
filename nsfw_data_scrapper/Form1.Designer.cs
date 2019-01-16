d8d534b3ebeb445233bc3fa4596a5fa5f7935100
liujun408 <liujun408@qq.com>
2019-01-16 16:10:25 +0800

    v1

---
+++ nsfw_data_scrapper/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace nsfw_data_scrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 397);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "drawings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "hentai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "neutral";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(298, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "porn";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(391, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "sexy";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(482, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "get all";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ nsfw_data_scrapper/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nsfw_data_scrapper
+{
+    public partial class Form1 : Form
+    {
+        int mutiThreds = 100;
+        int thredsCount = 0;
+        public List<imgUrl> urlList = new List<imgUrl>();
+
+        int downNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void getPicFromTxt(string type)
+        {
+
+
+            log("start download:" + type);
+
+            var path = "raw_data/" + type + "/urls_" + type + ".txt";
+            var saveFolder = "pics/" + type + "/";
+
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var urls = line.ToString();
+                string url = "", fileName = "";
+                var arr = urls.Replace("http://", "@").Split('@');
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != "")
+                    {
+                        url = "http://" + arr[i];
+                        fileName = url.Substring(url.LastIndexOf('/') + 1, url.Length - url.LastIndexOf('/') - 1);
+                        //if (!File.Exists(saveFolder + fileName))
+                        //{
+                        //    DoGetImage(url, saveFolder + fileName);
+                        //}
+                        imgUrl img = new imgUrl();
+                        img.url = url;
+                        img.path = saveFolder + fileName;
+                        if (!File.Exists(img.path))
+                        {
+                            urlList.Add(img);
+                        }
+                        
+
+                    }
+                }
+
+
+
+            }
+            log("url load success,total:" + urlList.Count);
+
+            timer1.Enabled = true;
+        }
+
+        public class imgUrl
+        {
+            public string url { get; set; }
+            public string path { get; set; }
+        }
+
+        public void DoGetImage()
+        {
+            var url = urlList[0].url;
+            var path = urlList[0].path;
+            urlList.RemoveAt(0);
+
+            if (File.Exists(path))
+            {
+                downNum++;
+                return;
+            }
+
+            thredsCount++;
+
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+                req.ServicePoint.Expect100Continue = false;
+                req.Method = "GET";
+                req.KeepAlive = true;
+
+                req.ContentType = "image/png";
+                HttpWebResponse rsp = (HttpWebResponse)req.GetResponse();
+
+                System.IO.Stream stream = null;
+
+                try
+                {
+                    // 以字符流的方式读取HTTP响应
+                    stream = rsp.GetResponseStream();
+                    Image.FromStream(stream).Save(path);
+                }
+                finally
+                {
+                    // 释放资源
+                    if (stream != null) stream.Close();
+                    if (rsp != null) rsp.Close();
+                }
+            }
+            catch (Exception err)
+            {
+                //log("error url:" + url);
+            }
+            thredsCount--;
+            downNum++;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getPicFromTxt("drawings");
+            timer2.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (thredsCount < mutiThreds && urlList.Count > 0)
+            {
+                System.Threading.Thread t = new System.Threading.Thread(DoGetImage);
+                t.Start();
+
+            }
+
+            if (urlList.Count == 0)
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        public void log(string s)
+        {
+            textBox1.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ":" + s + "\r\n");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            log("progress:" + downNum + "/" + urlList.Count + ",threds:" + thredsCount);
+            if (urlList.Count == 0)
+            {
+                log("all pics done");
+
+                timer2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getPicFromTxt("hentai");
+            timer2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getPicFromTxt("neutral");
+            timer2.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getPicFromTxt("porn");
+            timer2.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            getPicFromTxt("sexy");
+            timer2.Enabled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            getPicFromTxt("drawings");
+            getPicFromTxt("hentai");
+            getPicFromTxt("neutral");
+            getPicFromTxt("porn");
+            getPicFromTxt("sexy");
+            timer2.Enabled = true;
+        }
+    }
+}
